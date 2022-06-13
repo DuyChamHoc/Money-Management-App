8350a72f29cfa85f932f9a113a51146392fce7af
nguyendinhduy13 <your_email@example.com>
2022-06-14 01:43:37 +0700

    design

--- app/src/main/java/com/example/moneymanagementapp/LoanActivity.java
+++ app/src/main/java/com/example/moneymanagementapp/LoanActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
@@ -12,6 +14,7 @@ public class LoanActivity extends AppCompatActivity {
     VPAdapter vpAdapter;
     TabLayout tabLayout;
     ViewPager2 viewPager;
+    private ImageView arrow_back;
     private String[] title=new String[]{"Borrow","Lend"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +25,12 @@ public class LoanActivity extends AppCompatActivity {
         vpAdapter=new VPAdapter(this);
         viewPager.setAdapter(vpAdapter);
         new TabLayoutMediator(tabLayout,viewPager,((tab, position) -> tab.setText(title[position]))).attach();
+        arrow_back=findViewById(R.id.arrow_back);
+        arrow_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
