b86668ef76541a75a8e49b62f9b295d5da41896e
DuyChamHoc <100991730+DuyChamHoc@users.noreply.github.com>
2022-06-15 20:04:40 +0700

    update infor gg

--- app/src/main/java/com/example/moneymanagementapp/AccountActivity.java
+++ app/src/main/java/com/example/moneymanagementapp/AccountActivity.java
@@ -49,29 +49,28 @@ import java.util.Map;
 
 public class AccountActivity extends AppCompatActivity {
 
-    private ImageView img_avatar,img_camera;
-    private TextInputEditText edtFullName,phonenumber,birthday,tv_email;
+    private ImageView img_avatar, img_camera;
+    private TextInputEditText edtFullName, phonenumber, birthday, tv_email;
     private String a[];
     private Uri muri;
     private FirebaseAuth mAuth;
     private DatabaseReference budgetRef;
-    private Button btn_update,btn_cancel;
+    private Button btn_update, btn_cancel;
     private ProgressDialog progressDialog;
 
-    final private ActivityResultLauncher<Intent> mActivityResultLauncher=registerForActivityResult(
+    final private ActivityResultLauncher<Intent> mActivityResultLauncher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
                 @Override
                 public void onActivityResult(ActivityResult result) {
-                    if(result.getResultCode()==RESULT_OK)
-                    {
-                        Intent intent=result.getData();
-                        if(intent==null){
+                    if (result.getResultCode() == RESULT_OK) {
+                        Intent intent = result.getData();
+                        if (intent == null) {
                             return;
                         }
-                        Uri uri=intent.getData();
+                        Uri uri = intent.getData();
                         setMuri(uri);
                         try {
-                            Bitmap bitmap= MediaStore.Images.Media.getBitmap(getContentResolver(),uri) ;
+                            Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
                             img_avatar.setImageBitmap(bitmap);
                         } catch (IOException e) {
                             e.printStackTrace();
@@ -89,19 +88,18 @@ public class AccountActivity extends AppCompatActivity {
         img_camera = findViewById(R.id.img_camera);
         edtFullName = findViewById(R.id.edtFullName);
         tv_email = findViewById(R.id.tv_email);
-        phonenumber=findViewById(R.id.phonenumber);
-        birthday=findViewById(R.id.date);
+        phonenumber = findViewById(R.id.phonenumber);
+        birthday = findViewById(R.id.date);
         btn_update = findViewById(R.id.button_update);
         btn_cancel = findViewById(R.id.button_cancel);
-        mAuth=FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance();
         budgetRef = FirebaseDatabase.getInstance().getReference().child("Users").child(mAuth.getCurrentUser().getUid());
         budgetRef.child("birthday").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DataSnapshot> task) {
                 if (task.isSuccessful()) {
                     birthday.setText(String.valueOf(task.getResult().getValue()));
-                }
-                else {
+                } else {
                     Toast.makeText(AccountActivity.this, "return data false", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -110,21 +108,20 @@ public class AccountActivity extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<DataSnapshot> task) {
                 if (task.isSuccessful()) {
-                   phonenumber.setText(String.valueOf(task.getResult().getValue()));
-                }
-                else {
+                    phonenumber.setText(String.valueOf(task.getResult().getValue()));
+                } else {
                     Toast.makeText(AccountActivity.this, "return data false", Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
-        progressDialog=new ProgressDialog(this);
-        FirebaseUser user= FirebaseAuth.getInstance().getCurrentUser();
-        if(user==null){
+        progressDialog = new ProgressDialog(this);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user == null) {
             return;
         }
-        String email=user.getEmail();
-        if(user.getDisplayName()==null||user.getDisplayName().equals("")) {
+        String email = user.getEmail();
+        if (user.getDisplayName() == null || user.getDisplayName().equals("")) {
             budgetRef.child("fullname").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
                 @Override
                 public void onComplete(@NonNull Task<DataSnapshot> task) {
@@ -135,34 +132,34 @@ public class AccountActivity extends AppCompatActivity {
                     }
                 }
             });
-        budgetRef.child("email").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                if (task.isSuccessful()) {
-                    tv_email.setText(String.valueOf(task.getResult().getValue()));
-                } else {
-                    Toast.makeText(AccountActivity.this, "return data false", Toast.LENGTH_SHORT).show();
+            budgetRef.child("email").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DataSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        tv_email.setText(String.valueOf(task.getResult().getValue()));
+                    } else {
+                        Toast.makeText(AccountActivity.this, "return data false", Toast.LENGTH_SHORT).show();
+                    }
                 }
-            }
 
-        });
-        Uri photoUrl = user.getPhotoUrl();
-        Glide.with(this).load(photoUrl).error(R.drawable.ic_person_24).into(img_avatar);
-        muri = photoUrl;
-        showUserInformation1();
-        }
-        else{
-        tv_email.setText(email);
+            });
+            Uri photoUrl = user.getPhotoUrl();
+            Glide.with(this).load(photoUrl).error(R.drawable.ic_person_24).into(img_avatar);
+            muri = photoUrl;
+            showUserInformation1();
+        } else {
+            tv_email.setText(email);
             edtFullName.setText(user.getDisplayName());
             Uri photoUrl = user.getPhotoUrl();
             Glide.with(this).load(photoUrl).error(R.drawable.ic_person_24).into(img_avatar);
             muri = photoUrl;
-            showUserInformation();}
+            showUserInformation();
+        }
         initListener();
         btn_cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               finish();
+                finish();
             }
         });
 
@@ -193,10 +190,10 @@ public class AccountActivity extends AppCompatActivity {
     private void onClickUpdateProfile() {
         progressDialog.show();
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if(user==null){
+        if (user == null) {
             return;
         }
-        if(user.getDisplayName()==null||user.getDisplayName().equals("")){
+        if (user.getDisplayName() == null || user.getDisplayName().equals("")) {
             budgetRef.child("fullname").setValue(edtFullName.getText().toString().trim());
             budgetRef.child("phone").setValue(phonenumber.getText().toString().trim());
             budgetRef.child("birthday").setValue(birthday.getText().toString().trim());
@@ -213,31 +210,33 @@ public class AccountActivity extends AppCompatActivity {
                                 Toast.makeText(AccountActivity.this, "Update profile success", Toast.LENGTH_SHORT).show();
                                 showUserInformation1();
                             }
-                            Intent intent=new Intent(AccountActivity.this,ProfileActivity.class);
+                            Intent intent = new Intent(AccountActivity.this, ProfileActivity.class);
                             startActivity(intent);
                         }
                     });
-        }
-        else{
-        String strFullName=edtFullName.getText().toString().trim();
-        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
-                .setDisplayName(strFullName)
-                .setPhotoUri(muri)
-                .build();
-
-        user.updateProfile(profileUpdates)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        progressDialog.dismiss();
-                        if (task.isSuccessful()) {
-                            Toast.makeText(AccountActivity.this, "Update profile success", Toast.LENGTH_SHORT).show();
-                            showUserInformation();
+        } else {
+            String strFullName = edtFullName.getText().toString().trim();
+            String phone = phonenumber.getText().toString().trim();
+            String date = birthday.getText().toString().trim();
+            User user1=new User(strFullName,date,user.getEmail(),phone);
+            FirebaseDatabase.getInstance().getReference("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(user1);
+            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                    .setDisplayName("")
+                    .setPhotoUri(muri)
+                    .build();
+            user.updateProfile(profileUpdates)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            progressDialog.dismiss();
+                            if (task.isSuccessful()) {
+                                Toast.makeText(AccountActivity.this, "Update profile success", Toast.LENGTH_SHORT).show();
+                                showUserInformation();
+                            }
+                            Intent intent = new Intent(AccountActivity.this, ProfileActivity.class);
+                            startActivity(intent);
                         }
-                        Intent intent=new Intent(AccountActivity.this,ProfileActivity.class);
-                        startActivity(intent);
-                    }
-                });
+                    });
         }
 
     }
@@ -251,24 +250,25 @@ public class AccountActivity extends AppCompatActivity {
     }
 
     private void openGallery() {
-        Intent intent=new Intent();
+        Intent intent = new Intent();
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
-        mActivityResultLauncher.launch(Intent.createChooser(intent,"Select Picture"));
+        mActivityResultLauncher.launch(Intent.createChooser(intent, "Select Picture"));
     }
 
-    private void showUserInformation(){
-        FirebaseUser user= FirebaseAuth.getInstance().getCurrentUser();
-        if(user==null){
+    private void showUserInformation() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user == null) {
             return;
         }
         Uri photoUrl = user.getPhotoUrl();
         edtFullName.setText(user.getDisplayName());
         Glide.with(this).load(photoUrl).error(R.drawable.ic_person_24).into(img_avatar);
     }
-    private void showUserInformation1(){
-        FirebaseUser user= FirebaseAuth.getInstance().getCurrentUser();
-        if(user==null){
+
+    private void showUserInformation1() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user == null) {
             return;
         }
         Uri photoUrl = user.getPhotoUrl();
